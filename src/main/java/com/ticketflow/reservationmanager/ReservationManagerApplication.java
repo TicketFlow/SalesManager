8afb255226c75064d912e7bfbaeaca2be15261d4
daniellygj <daniellygj@gmail.com>
2023-03-02 20:01:25 -0300

    copnfigurações iniciais #2

---
+++ src/main/java/com/ticketflow/reservationmanager/ReservationManagerApplication.java
@@ -0,0 +1,15 @@
+package com.ticketflow.reservationmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ReservationManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticketflow/reservationmanager/ReservationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketflow.reservationmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
